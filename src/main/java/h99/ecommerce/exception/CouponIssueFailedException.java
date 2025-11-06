26d15fb4b9c5b0881ab1b20955685f2a899b849c
jiminkim <ckwkek@gmail.com>
2025-11-07 00:33:28 +0900

    feat: custom exception 추가

---
+++ src/main/java/h99/ecommerce/exception/CouponIssueFailedException.java
@@ -0,0 +1,7 @@
+package h99.ecommerce.exception;
+
+public class CouponIssueFailedException extends RuntimeException {
+    public CouponIssueFailedException(String message) {
+        super(message);
+    }
+}
