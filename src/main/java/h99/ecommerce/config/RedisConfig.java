22a87662556fbc64f903e561bd1f4f2769016a7b
jiminkim <ckwkek@gmail.com>
2025-11-28 02:10:45 +0900

    fix: redisConfig 어노테이션 수정

--- src/main/java/h99/ecommerce/config/RedisConfig.java
+++ src/main/java/h99/ecommerce/config/RedisConfig.java
@@ -5,12 +5,10 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
